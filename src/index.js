23b9e056acdb8096f38fe29259a996c7dd2cd5e8
gabbersepp <gabbersepp@googlemail.com>
2020-12-22 09:46:13 +0100

    forgot another change for passing options

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const reader = require("./reader");
 const github = require("./github");
 const cloud = require("./get-cloud");
 
-async function generateCloud(githubName, githubToken, extensions, outputFolder, skipRation, optionsToMerge) {
+async function generateCloud(githubName, githubToken, extensions, outputFolder, skipRation, optionsToMerge, puppeteerOptions) {
     optionsToMerge = optionsToMerge || {};
     if (!outputFolder) {
         outputFolder = path.join(process.cwd(), "output");
@@ -16,7 +16,7 @@ async function generateCloud(githubName, githubToken, extensions, outputFolder, 
 
     await github.fetchRepositories(outputFolder, githubName, githubToken);
     await reader.readAndSaveAll(outputFolder, extensions, skipRation);
-    await cloud.saveImageAndHtml(outputFolder, optionsToMerge);
+    await cloud.saveImageAndHtml(outputFolder, optionsToMerge, puppeteerOptions);
 }
 
 //generateCloud("gabbersepp", process.env.GITHUB_TOKEN, ["js", "asm", "cs", "ts", "java", "cpp"],  path.join(process.cwd(), "temp") , 0.8, { color: "black" });
